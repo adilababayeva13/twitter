2c6ae7454224857fec93626095e71162acdf2d11
adilababyeva13 <adilababayeva13@gmail.com>
2021-04-24 00:16:06 +0400

    bezi problemler hell olundu

--- assets/js/SignUp.js
+++ assets/js/SignUp.js
@@ -2,55 +2,76 @@ $(document).ready(function(){
 
     
     function ChangeColor(){
-        $("#name").removeClass("change2");
-    $("#name").addClass("change");
-   
+   var c = $("#name").val();
+    $("#span-name").removeClass("label-name");
+    $("#span-name").addClass("change-label-name");
+    $("#span-name").css("color","#1da1f2");
+    $("#span-length").html(c.length + "/ 50"); 
+
     $("#name").css("border-style", "groove");
     $("#name").css("border-width", "3px");
-    
+    $("#name").css("border-color", "rgb(107, 177, 241)");
+    $("#name").css("border-radius", "5px");
+    $("#name").css("box-shadow", "0px 0px 2px rgb(51, 124, 192)");
 
     }
+
     function Default(){
-        
-        $("#name").addClass("change2");
+        var c = $("#name").val();
+        if(c.trim() == "" ){
+            $("#span-name").removeClass("change-label-name");
+            $("#span-name").addClass("label-name");
+        }
+        $("#span-name").css("color","gray");
+        $("#span-length").html("");
        
-        $("#name").css("border-style", "solid");
-        $("#name").css("border-width", "1px");
-
-
+        $("#name").css("border", "solid rgb(184, 182, 182) 1px");
+        $("#name").css("border-radius", "5px");
+        $("#name").css("box-shadow", "0px 0px 0px rgb(51, 124, 192)");
+        $("#nameHelp").text("");
         }
 
     function ChangeColor2(){
-        $("#number").removeClass("change2");
-        $("#number").addClass("change");
+        $("#span-phone").removeClass("label-phone");
+        $("#span-phone").addClass("change-label-phone");
+        $("#span-phone").css("color","#1da1f2");
+
+        $("#number").css("border-style", "groove");
+        $("#number").css("border-width", "3px");
+        $("#number").css("border-color", "rgb(107, 177, 241)");
+        $("#number").css("border-radius", "5px");
+        $("#number").css("box-shadow", "0px 0px 2px rgb(51, 124, 192)");
         }
     function Default2(){
-            $("#number").addClass("change2");
+        $("#span-phone").removeClass("change-label-phone");
+        $("#span-phone").addClass("label-phone");
+        $("#span-phone").css("color","gray");
             $("#number").css("border-style", "solid");
             $("#number").css("border-width", "1px");
-             $("#number").css("box-shadow", "0px 0px 0px red");
-             $("#number").css("border-color", "rgb(184, 182, 182) ");
-             $("#telHelp").text("");
-             $("#number").removeClass("change3");
-            
+            $("#number").css("box-shadow", "0px 0px 0px red");
+            $("#number").css("border-color", "rgb(184, 182, 182) ");
+            $("#telHelp").text("");
             }
            
      function ChangeColor3(){
     var c = $("#name").val();
     
-        
+    $("#span-length").html(c.length + "/ 50"); 
         if(c.trim() == "" ){
            
             $("#name").css("box-shadow", "0px 0px 1px red");
             $("#name").css("border-color", "red");
-            $("#name").addClass("change3");
+            $("#span-name").css("color", "red");
             $("#nameHelp").text("What's your name?");
+            $("#span-length").html("0 / 50");
         }
 
         else{
+            $("#span-name").css("color", "#1da1f2");
             $("#name").css("box-shadow", "0px 0px 3px #45a9ec");
             $("#name").css("border-color", "#45a9ec");
             $("#nameHelp").text("");
+           
         }
             
             }
@@ -60,90 +81,55 @@ $(document).ready(function(){
     function ChangeColor1(e){
     
      var place=$("#number").val();
-     var array=[];
-     array.push(e.key);
+    var array=[];
+    array.push(e.key);
     
     var letters=[ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z','[',']',';','{', '}',':','"','<','>', ',','.','/','?',"'"];
 
     var numbers=['0','1', '2', '3', '4', '5', '6', '7', '8','9'];
     
-    for(var i=0; i<letters.length; i++){
+    
         
-    if(array.indexOf(letters[i]) !== -1){
+    if(letters.indexOf(e.key) !== -1){
         $("#number").css("border-color", "red");
-        $("#number").css("box-shadow", "0px 0px 3px red");
         $("#telHelp").text("Please enter a valid phone number.");
-        $("#number").addClass("change3");
-    }}
-      
-    for(var k=0; k<numbers.length; k++){
-        if(array.indexOf(numbers[k]) !== -1){
-            $("#number").css("border-color", "#45a9ec");
-            $("#number").css("box-shadow", "0px 0px 3px #45a9ec");
-            $("#telHelp").text("");
-        }}
+        $("#span-phone").css("color","red");
+    }
+    
 
+   
+    if(numbers.indexOf(e.key) !== -1 || e.which == 8){
+   
+        for(var i=0; i<array.lenght ;i++){
+            if(letters.indexOf(array[i]) == -1){
+            $("#number").css("border-color", "rgb(107, 177, 241)");
+            $("#telHelp").text("");
+            $("#span-phone").css("color","#45a9ec");
+        }
+    else{
+        $("#number").css("border-color", "red");
+        $("#telHelp").text("Please enter a valid phone number.");
+        $("#span-phone").css("color","red");
+    }}}
+
+         
+    if(place ==""){
+        $("#number").css("border-color", "rgb(107, 177, 241)");
+            $("#telHelp").text("");
+            $("#span-phone").css("color","#45a9ec");
+    }
+        
     
 
     }        
    
-   
-   
-
-    function removeOptions(){  //removes options call each time the day selector is changed
-      
-   
-   var monthselected=$("#monthSelect");
-   var feb=$("#monthSelect option[value='02']");
-   var apr=$("#monthSelect option[value='04']");
-   var june=$("#monthSelect option[value='06']");
-   var sep=$("#monthSelect option[value='09']");
-   var nov=$("#monthSelect option[value='11']");
-   
-
-
-    if(monthselected == feb){
-    
         
-        $("#dayselect option[value='31']").remove();
-    }
-    if(monthselected == apr){
-    
-      
-        
-        $("#dayselect option[value='31']").remove();
-    }
-    if(monthselected == june){
-    
-       
-        
-        $("#dayselect option[value='31']").remove();
-    }
-    if(monthselected == sep){
-    
-       
-        
-        $("#dayselect option[value='31']").remove();
-    }
-    if(monthselected == nov){
-    
-      
-        
-        $("#dayselect option[value='31']").remove();
-    }
-
-      
-    }
-
-   
-        
-    
-    function SignValidator(){
+        function SignValidator(){
         var a = $("#name").val();
         var b =$("#number").val();
       
         
-        if(a.trim() !== "" && b.trim() !== "" && !($("#month option[value='00']"))&& !($("#day option[value='00']"))&& !($("#year option[value='00']")) ){
+        if(a.trim() !== "" && b.trim() !== "" ){
             $("#button").attr({
                 disabled: false
             });
@@ -170,16 +156,6 @@ $(document).ready(function(){
     $("#number").on('blur', Default2);
     $("#name").on('input', ChangeColor3);
     $("#number").on('keyup', ChangeColor1);
-    $("option").on('change',removeOptions)
     
-    function text(e){
-        var letter =e.key;
-        var input=[];
-        input.push(letter);
-        
-         $("#name").val( input +"/50");
-     }
- 
-   
-
+    
 });
